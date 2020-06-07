a46b10b532fee3f116df67f4da08e3c8af50f652
deadro22 <44213280+deadro22@users.noreply.github.com>
2020-06-07 19:41:57 +0100

    ch_w

--- DATABASE/MongoSetup.js
+++ DATABASE/MongoSetup.js
@@ -6,12 +6,12 @@ const question = new mongoose.Schema({
   definition: [{ type: String, required: true }],
   link: { type: String },
   image: { type: String },
-  voice: [
+  voice: 
     {
       vc: { type: Buffer, required: true },
       vc_Type: { type: String, required: true },
     },
-  ],
+  ,
 });
 const tag = mongoose.Schema({
   tag: { type: String, required: true, lowercase: true },
--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 mongoose
-  .connect(process.env.DR_URI, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
